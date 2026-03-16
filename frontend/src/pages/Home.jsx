810bad4cae9a0882130d62329c4ae711ce1b9c8b
Anuj <anujn158@gmail.com>
2026-03-16 22:01:47 +0530

    updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -104,7 +104,7 @@ const Home = () => {
 
     return (
         <div className="w-full bg-black min-h-screen text-white p-6">
-            <h1 className="text-4xl text-center text-white font-bold">Welcome back - {username} </h1>
+            <h1 className="md:text-4xl sm:text-2xl text-xl text-center text-white font-bold">Welcome back - {username} </h1>
             <div className="bg-gray-900 p-4 mt-5 shadow-md rounded-lg">
                 <form onSubmit={handleSubmit} className="flex flex-col gap-2">
                     <textarea
